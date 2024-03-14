42f38ffa29115c7982602f7a016382aefd080dd4
JackieChheng <jackiechheng4@gmail.com>
2024-03-13 18:09:42 -0600

    fixed components

--- tetris-reactjs/src/components/Tetris.js
+++ tetris-reactjs/src/components/Tetris.js
@@ -1,22 +1,25 @@
 import React from 'react';
 
+
+// Components
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
-
-
     return (
         <div>
             <Stage />
-            <aisde>
+            <aside>
+                <div>
                 <Display text="Score" />
                 <Display text="Rows" />
                 <Display text="Levels" />
-
-
-            </aisde>
+                </div>
+                <StartButton />
+            </aside>
         </div>
-    )
-}
+    );
+};
+
+export default Tetris;
